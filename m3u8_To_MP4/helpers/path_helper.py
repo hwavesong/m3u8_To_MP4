2893dde89c5629ce47a568d086e29279780663c3
songs18 <songhaohao2018@cqu.edu.cn>
2021-10-24 11:19:56 +0800

    Rename utils.py -> path_helper.py

--- m3u8_To_MP4/helpers/path_helper.py
+++ m3u8_To_MP4/helpers/path_helper.py
@@ -1,69 +1,19 @@
 # -*- coding: utf-8 -*-
 import datetime
-import logging
-import os
-import re
-import sys
+from collections import Counter
 
 
-class ProcessBar:
-    def __init__(self, progress, max_iter, prefix='Progress',
-                 suffix='downloading',
-                 completed_suffix='completed', bar_length=50):
-        self.progress = progress
-        self.max_iter = max_iter
-
-        self.bar_length = bar_length
-
-        self.prefix = prefix
-        self.suffix = suffix
-
-        self.completed_suffix = completed_suffix
-
-    def display(self):
-        progress_rate = self.progress / self.max_iter
-
-        percent = 100 * progress_rate
-
-        filled_length = round(self.bar_length * progress_rate)
-        bar = '#' * filled_length + '-' * (self.bar_length - filled_length)
-
-        sys.stdout.write(
-            '\r{}: |{}| {:.1f}% {}'.format(self.prefix, bar, percent,
-                                           self.suffix))
-
-        if self.progress == self.max_iter:
-            sys.stdout.write(
-                '\r{}: |{}| {:.1f}% {}'.format(self.prefix, bar, percent,
-                                               self.completed_suffix))
-            sys.stdout.write('\n')
-
-        sys.stdout.flush()
-
-    def update(self):
-        self.progress += 1
-
-        self.display()
+def updated_resource_path(path, query):
+    if query:
+        path = f'{path}?{query}'
+    return path
 
 
 def resolve_file_name_by_uri(uri):
     # pattern = r"\/+(.*)"
     # file_name = re.findall(pattern=pattern, string=uri)[0]
-    file_name=uri.split('/')[-1]
+    file_name = uri.split('/')[-1]
     return file_name
-
-
-def display_speed(start_time, fetch_end_time, task_end_time,
-                  target_mp4_file_path):
-    download_time = fetch_end_time - start_time
-    total_time = task_end_time - start_time
-
-    download_speed = os.path.getsize(
-        target_mp4_file_path) / download_time / 1024
-
-    logging.info(
-        "download successfully！ take {:.2f}s,  average download speed is {:.2f}KB/s".format(
-            total_time, download_speed))
 
 
 def calibrate_mp4_file_name(mp4_file_name):
@@ -75,10 +25,23 @@ def calibrate_mp4_file_name(mp4_file_name):
     for ch in banned_ls:
         mp4_file_name = mp4_file_name.replace(ch, '')
 
+    if not mp4_file_name.endswith('.mp4'):
+        mp4_file_name += '.mp4'
+
     return True, mp4_file_name
 
 
 def create_mp4_file_name():
-    mp4_file_name = 'm3u8_To_Mp4_{}.mp4'.format(
-        datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
+    mp4_file_name = 'm3u8_To_Mp4_{}.mp4'.format(datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S'))
     return mp4_file_name
+
+
+def longest_common_subsequence(segment_absolute_urls):
+    num_shortest_segment_absolute_url_length = min(len(url) for url in segment_absolute_urls)
+
+    common_subsequence = list()
+    for i in range(num_shortest_segment_absolute_url_length):
+        c = Counter(segment_absolute_url[i] for segment_absolute_url in segment_absolute_urls)
+        common_subsequence.append(c.most_common(1)[0][0])
+
+    return ''.join(common_subsequence)
