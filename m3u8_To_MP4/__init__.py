046c15fd6975ef5005a84a3341e7f1e673349a26
songs18 <songhaohao2018@cqu.edu.cn>
2021-10-25 11:01:21 +0800

    Compatible with old API;

--- m3u8_To_MP4/__init__.py
+++ m3u8_To_MP4/__init__.py
@@ -42,7 +42,7 @@ __all__ = (
 )
 
 
-def download(m3u8_uri, max_retry_times=3, num_concurrent=50, mp4_file_dir=None, mp4_file_name='m3u8_To_Mp4.mp4', tmpdir=None):
+def async_download(m3u8_uri, max_retry_times=3, num_concurrent=50, mp4_file_dir=None, mp4_file_name='m3u8_To_Mp4.mp4', tmpdir=None):
     '''
     Download mp4 video from given m3u uri.
 
@@ -56,3 +56,33 @@ def download(m3u8_uri, max_retry_times=3, num_concurrent=50, mp4_file_dir=None, 
 
     with m3u8_To_MP4.async_processor.Crawler(m3u8_uri, max_retry_times, num_concurrent, mp4_file_dir, mp4_file_name, tmpdir) as crawler:
         crawler.fetch_mp4_by_m3u8_uri(True)
+
+
+def multithread_download(m3u8_uri, max_retry_times=3, max_num_workers=100, mp4_file_dir='./', mp4_file_name='m3u8_To_Mp4.mp4', tmpdir=None):
+    '''
+    Download mp4 video from given m3u uri.
+    :param m3u8_uri: m3u8 uri
+    :param max_retry_times: max retry times
+    :param max_num_workers: number of download threads
+    :param mp4_file_dir: folder path where mp4 file is stored
+    :param mp4_file_name: a mp4 file name with suffix ".mp4"
+    :return:
+    '''
+    with m3u8_To_MP4.sync_processor.Crawler(m3u8_uri, max_retry_times, max_num_workers, mp4_file_dir, mp4_file_name, tmpdir) as crawler:
+        crawler.fetch_mp4_by_m3u8_uri()
+
+import warnings
+
+def download(m3u8_uri, max_retry_times=3, max_num_workers=100, mp4_file_dir='./', mp4_file_name='m3u8_To_Mp4.mp4', tmpdir=None):
+    '''
+    Download mp4 video from given m3u uri.
+    :param m3u8_uri: m3u8 uri
+    :param max_retry_times: max retry times
+    :param max_num_workers: number of download threads
+    :param mp4_file_dir: folder path where mp4 file is stored
+    :param mp4_file_name: a mp4 file name with suffix ".mp4"
+    :return:
+    '''
+    warnings.warn('download function is deprecated, and please use multithread_download.',DeprecationWarning)
+
+    multithread_download(m3u8_uri, max_retry_times, max_num_workers, mp4_file_dir, mp4_file_name, tmpdir)
