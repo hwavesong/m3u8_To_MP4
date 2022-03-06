52a2053e9a51b1c2c34f8a6b2e80e06c8e9be7a3
songs18 <songhaohao2018@cqu.edu.cn>
2022-03-06 11:25:30 +0800

    Fix resolving addr infos when url contains port.

--- m3u8_To_MP4/networks/synchronous/sync_DNS.py
+++ m3u8_To_MP4/networks/synchronous/sync_DNS.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import socket
 import urllib.parse
 
@@ -8,12 +9,18 @@ from m3u8_To_MP4.networks.http_base import AddressInfo
 def available_addr_infos_of_url(url):
     scheme, netloc, path, query, fragment = urllib.parse.urlsplit(url)
 
+    specific_port_pattern = re.compile(r'(:\d+)')
+    specific_ports = re.findall(specific_port_pattern, netloc)
+
+    netloc = re.sub(specific_port_pattern, '', netloc)
+
     # todo:: support IPv6
     addr_infos = socket.getaddrinfo(host=netloc, port=scheme, family=socket.AF_INET)
 
     available_addr_info_pool = list()
     for family, type, proto, canonname, sockaddr in addr_infos:
-        ai = AddressInfo(host=sockaddr[0], port=sockaddr[1], family=family, proto=proto)
+        port = specific_ports[0] if len(specific_ports) > 0 else sockaddr[1]
+        ai = AddressInfo(host=sockaddr[0], port=port, family=family, proto=proto)
         available_addr_info_pool.append(ai)
 
     return available_addr_info_pool
